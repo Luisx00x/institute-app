817ffcb3b45f4b01315b70f5f5bd7a41271f0849
Luis Marcano <lmarcano203@gmail.com>
2023-07-09 12:06:43 -0500

    fixing useRouter warning

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,21 @@
 import Login from '@/components/login/Login';
 import { useSelector } from 'react-redux';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
 
   const user = useSelector(state => state.primarySlice.userLog);
   const router = useRouter();
 
+  useEffect( () => {
+    if(user.RolId === 1){
+      router.push("/Admin");
+    }
+  },[user])
+
   return (
     <>
-        { user.RolId === 1 ? router.push('/Admin') : null}
           <Login />
     </>      
   )
