b35c21836d37e9083213ffb619975a91ab09d49a
Luis Marcano <lmarcano203@gmail.com>
2023-07-11 23:51:47 -0500

    changing links styles

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,27 @@
 
 import Link from 'next/link';
 import s from './Sidebar.module.css';
+import { usePathname } from 'next/navigation';
 
 const Sidebar = ({menus}) => {
+
+  const pathname = usePathname();
 
   return (
     <nav className={s.container}>
       <ul className={s.list}>
           {
             menus.map( (menu,index) => {
-              return <li key={index}><Link href={menu.route} >{menu.name}</Link></li>
+              return (
+              <li key={index}>
+                <Link 
+                className={`${s.link} ${ pathname == menu.route ? s.active : "" }`} 
+                href={menu.route}
+                >
+                    {menu.name}
+                </Link>
+              </li>
+              )
             })
           }
         </ul>
