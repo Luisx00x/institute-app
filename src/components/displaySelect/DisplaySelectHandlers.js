9091c43a40147f153a3497b4b48734dc382fbb0e
Luis Marcano <lmarcano203@gmail.com>
2023-07-16 15:57:47 -0500

    adding new action to redux to set Students state and changin DisplaySelectHandler to parse the respose to int

--- src/components/displaySelect/DisplaySelectHandlers.js
+++ src/components/displaySelect/DisplaySelectHandlers.js
@@ -2,16 +2,16 @@ export const setSelectHandler = (e, set) => {
 
   e.preventDefault();
   set( prev => {
-    console.log(e.target.name)
+    console.log(e.target.value, "DENRO DE FUNCIOn")
     if(e.target.name !== "gradeId"){
       return {
         ...prev,
-        [e.target.name]: e.target.value,
+        [e.target.name]: parseInt(e.target.value),
       }
     }
     return {
       ...prev,
-      [e.target.name]: e.target.value,
+      [e.target.name]: parseInt(e.target.value),
       sectionId: null
     }
   })
--- src/redux/slice.js
+++ src/redux/slice.js
@@ -17,7 +17,8 @@ export const AdminSlice = createSlice({
   name: "Admin",
   initialState: {
     allGrades: [],
-    Teachers: []
+    Teachers: [],
+    Students: []
   },
   reducers: {
     setAllGrades: (state, action) => {
@@ -25,10 +26,13 @@ export const AdminSlice = createSlice({
     },
     setTeachers: (state, action) => {
       state.Teachers = action.payload;
+    },
+    setStudents: (state, action) => {
+      state.Students = action.payload;
     }
   }
 });
 
 export const { setUser } = Slice.actions;
 
-export const { setAllGrades, setTeachers } = AdminSlice.actions;
+export const { setAllGrades, setTeachers, setStudents } = AdminSlice.actions;
