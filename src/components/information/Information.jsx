ea245504facc9e6c7c55e3f800c79206143160a2
Luis Marcano <lmarcano203@gmail.com>
2023-07-17 00:27:59 -0500

    adding components to show information of the database

--- src/components/information/Information.jsx
+++ src/components/information/Information.jsx
@@ -1,9 +1,10 @@
+import ShowStudents from '../showStudents/ShowStudents';
 import s from './Information.module.css';
 
 const Information = () => {
   return (
     <div className={s.container}>
-      
+      <ShowStudents />
     </div>
   )
 }
