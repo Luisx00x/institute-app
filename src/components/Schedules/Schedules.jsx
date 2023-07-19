a426adfbe4147e5ccefa7c2f77cecb233508e1db
Luis Marcano <lmarcano203@gmail.com>
2023-07-19 15:18:06 -0500

    creating component to show Schedules

---
+++ src/components/Schedules/Schedules.jsx
@@ -0,0 +1,19 @@
+import s from './Schedules.module.css';
+
+const Schedules = () => {
+  return (
+    <>
+      <div className={s.gridContainer}>
+        <div className={s.gridLeft}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta voluptatibus dolorem iure vero exercitationem quibusdam suscipit, quaerat aperiam sed aut accusantium totam at error quod ut eveniet doloremque. Velit, non.
+        </div>
+        <div className={s.gridRight}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores culpa non rerum praesentium ab cumque, facere aspernatur sunt laborum tempora aliquam veniam! Sint aut aperiam cupiditate sed possimus ad. Iure?
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default Schedules;
