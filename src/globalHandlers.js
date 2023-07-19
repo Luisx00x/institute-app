42c7cae4ccc69e2c70117c6f88c796e89e7a4e5b
Luis Marcano <lmarcano203@gmail.com>
2023-07-19 15:17:41 -0500

    adding a function to take teacher information

--- src/globalHandlers.js
+++ src/globalHandlers.js
@@ -31,52 +31,23 @@ export const searchInformation = (path, dispatch) =>{
 
 }
 
-export const searchIndividual = (id, rol, applying, dispatch) =>{
+export const setTeacherInformation = (user, rol, applicant, year, dispatch) => {
 
   let status;
 
-  useFetch(`${URL_LOGIN}/queries/searchOneUser?user=${id}&rol=${rol}&applicant=${applying}`, "GET")
-  .then( res => {
-    status = res.status;
-    return res.json();
-  })
-  .then( res => {
-    if(status !== 200){
-      const error = {
-        msg: res.message,
-        title: res.name,
-        isActive: true,
-        type: FAILURE
+    useFetch(`${URL_LOGIN}/teacher/yearGrades?user=${user}&rol=${rol}&applicant=${applicant}&year=${year}`, "GET")
+    .then( res => {
+      status = res.status;
+      return res.json();
+    })
+    .then( res => {
+      if(status !== 200){
+        throw new Error(res);
       }
-      dispatch(setModal(error));
-      throw new Error(res)
-    }
-    /* dispatch(setCourses(res)); */
-    if(parseInt(rol) === 3){
-
-      const {id, name, lastName, email} = res.Teacher;
-      const { Courses } = res.Teacher;
-      let sections = [];
-      let grades = [];
-
-      const courses = Courses.map( course => {
-        if(!sections.find(ele => ele.id === course.Section.id)) {
-          sections.push({ id:course.Section.id, sectionName: course.Section.sectionName });
-        }
-        if(!grades.find(ele => ele.id === course.Section.Grade.id)){
-          grades.push(course.Section.Grade);
-        }
-        return { id: course.id, courseName: course.courseName }
-      });
-
-      dispatch(setTeacher({id, name, lastName, email}));
-      dispatch(setCourses(courses));
-      dispatch(setSections(sections));
-      dispatch(setGrades(grades));
-    }
-  })
-  .catch(err => {
-    console.error(err)
-  })
+      dispatch(setCourses(res));
+    })
+    .catch( err => {
+      console.error(err);
+    })
 
 }
