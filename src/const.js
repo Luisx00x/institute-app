ea28b5d8a30f65817e24621236d32e649d0ca672
Luis Marcano <lmarcano203@gmail.com>
2023-07-22 14:03:25 -0500

    adding new const

--- src/const.js
+++ src/const.js
@@ -6,5 +6,8 @@ export const SUCCESS = "success";
 export const FAILURE = "failure";
 
 export const HOMEWORKS = "homeworks";
+export const CLASSES = "classes";
 
 export const DAYS = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+export const CLASSLIST = {col1: "Clase", col2: "Archivo"};
+export const HOMEWORKSLIST = {col1: "Asignación", col2:"Archivo"};
