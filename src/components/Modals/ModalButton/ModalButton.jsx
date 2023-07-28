53b4530c4dfd50d0347292b820187a7e521c3e48
Luisx00x <lmarcano203@gmail.com>
2023-07-28 15:06:39 -0500

    adding modalButton option to submit files

--- src/components/Modals/ModalButton/ModalButton.jsx
+++ src/components/Modals/ModalButton/ModalButton.jsx
@@ -2,10 +2,11 @@
 
 import s from './ModalButton.module.css';
 import { useDispatch } from "react-redux";
-import { confirmModal, submitInfo } from "./ModalButtonHandler";
+import { confirmModal, submitInfo, submitReleaseFile } from "./ModalButtonHandler";
 import { SUCCESS } from '@/const';
+import { submitFile } from '@/components/UploadFiles/uploadHandler';
 
-const ModalButton = ({text, type, data, url}) => {
+const ModalButton = ({text, type, data, setData, url, uploadFile, input, setInput}) => {
 
   const dispatch = useDispatch();
 
@@ -14,10 +15,13 @@ const ModalButton = ({text, type, data, url}) => {
       <button
       className={ type === SUCCESS ? s.success : s.failure}
       onClick={(e) => {
-        if(data) submitInfo(e, data, dispatch, url)
+
+        if(data && uploadFile == false) submitInfo(e, data, dispatch, url);
+        if(data && uploadFile == true) submitReleaseFile(e, data, input, dispatch, setData, setInput, url);
         else{
           confirmModal(e, dispatch)
         }
+      
       }}
       >{text}</button>
     </>
--- src/components/Modals/ModalButton/ModalButtonHandler.js
+++ src/components/Modals/ModalButton/ModalButtonHandler.js
@@ -1,6 +1,8 @@
 import useFetch from "@/Hooks/useFetch";
+import { FAILURE, SUCCESS } from "@/const";
 import { setModal } from "@/redux/slice";
 const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL;
+const CREATE_RELEASE = process.env.NEXT_PUBLIC_CREATE_RELEASE;
 
 const confirm = {
   isActive: false,
@@ -32,6 +34,72 @@ export const submitInfo = (e, body, dispatch, url) => {
     if(res === 400 || res === 404) throw new Error(res)
     console.log(res);
     dispatch(setModal(confirm))
+  })
+
+}
+
+export const getReleaseFile = (e) => {
+  const [ file ] = e.target.files;
+ 
+  const fileTemp = {
+    fileRaw: file,
+    fileName: file.name
+  }
+  return fileTemp;
+}
+
+export const submitReleaseFile = (e, data, input, dispatch, setData, setInput, url) => {
+  
+  if(!data) return
+
+  const sendUrl = `${LOGIN_URL}/${url}`
+  
+  let status
+
+  const body = new FormData();
+
+  body.append('newRelease', data.fileRaw, data.fileName);
+
+  body.append('title', input.title);
+  body.append('sender', input.sender);
+  body.append('userRol', input.userRol);
+  body.append('sectionId', input.sectionId);
+
+  e.preventDefault();
+  useFetch(`${sendUrl}`, "POST", body, true)
+  .then(res => {
+    status = res.status;
+    return res.json();
+  })
+  .then( res => {
+    if(status === 200){
+      const modalBody = {
+        isActive: true,
+        type: SUCCESS,
+        title: "Envio de tarea",
+        msg: res
+      }
+      setData(prev => undefined);
+      setInput(prev => {
+        return {
+          ...prev,
+          inputValue: ""
+        }
+      });
+      return dispatch(setModal(modalBody));
+    }
+    throw new Error(res);
+  })
+  .catch(err => {
+    const errorBody = {
+      isActive: true,
+      type: FAILURE,
+      title: err.name,
+      msg: err.message
+    }
+    dispatch(setModal(errorBody));
+    setData(prev => undefined);
+    setInput(prev => "");
   })
 
 }
