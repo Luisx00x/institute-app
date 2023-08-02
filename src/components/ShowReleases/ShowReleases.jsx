fa7c486f32c74c0518b14c03a565939efa74cf06
Luisx00x <lmarcano203@gmail.com>
2023-08-02 07:44:07 -0500

    change in component to render different field based in parameters

--- src/components/ShowReleases/ShowReleases.jsx
+++ src/components/ShowReleases/ShowReleases.jsx
@@ -2,34 +2,51 @@
 import s from './ShowReleases.module.css';
 const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL;
 
-const ShowReleases = ({releases}) => {
+const ShowReleases = ({releases, courseReleases}) => {
+
+  const render = releases || courseReleases;
 
   return (
     <>
 
       <div className={s.releasesContainer}>
 
-        <section className={s.gridContainer}>
+        <section className={releases ? s.gridContainer : s.gridCourseContainer}>
 
           <div className={s.gridCol1}>Enviado por:</div>
 
           <div className={s.gridCol2}>Título:</div>
 
-          <div className={s.gridCol3}>Archivo:</div>
+          {
+            courseReleases
+            ?
+            <div className={s.gridCol3}>Curso: </div>
+            :
+            null
+          }
 
-          <div className={s.gridCol4}>Fecha</div>
+          <div className={releases ? s.gridCol3 : s.gridCol4}>Archivo:</div>
+
+          <div className={releases ? s.gridCol4 : s.gridCol5}>Fecha</div>
 
           {
-            releases?.map( release => {
+            render?.map( release => {
               return (
                 <>
 
                   <div className={s.gridCol1}>{release.sender}</div>
                   <div className={s.gridCol2}>{release.title}</div>
-                  <div className={s.gridCol3}>
+                  {
+                    courseReleases
+                    ?
+                    <div className={s.gridCol3}>{release.Course.courseName}</div>
+                    :
+                    null
+                  }
+                  <div className={releases ? s.gridCol3 : s.gridCol4}>
                     <a href={`${LOGIN_URL}/${release.location}`} target='_blank'>Abrir</a>
                   </div>
-                  <div className={s.gridCol4}>{release.createdAt.slice(0,10)}</div>
+                  <div className={releases ? s.gridCol4 : s.gridCol5}>{release.createdAt.slice(0,10)}</div>
 
                 </>
               )
