d38def3ff1481ac8600009587157a6b3c6530c9a
Luisx00x <lmarcano203@gmail.com>
2023-08-02 07:44:46 -0500

    change in parameters passed to ShowReleases

--- src/app/student/releases/page.jsx
+++ src/app/student/releases/page.jsx
@@ -1,15 +1,18 @@
 'use client'
 import ShowReleases from "@/components/ShowReleases/ShowReleases";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { searchReleases } from "./releasesHandlers";
+import TabMenu from "@/components/TabMenu/TabMenu";
 
 const StudentReleases = () => {
   
   const dispatch = useDispatch();
   const releases = useSelector(state => state.student.releases);
-  const studentId = useSelector(state => state.student.studentInfo.id);
-  const sectionId = useSelector(state => state.student.sectionInfo.id);
+  const studentId = useSelector(state => state.student.studentInfo?.id);
+  const sectionId = useSelector(state => state.student.sectionInfo?.id);
+  const [activeTab, setActiveTab] = useState(1);
+  const tabOptions = ["Secciones", "Cursos", "Personales"]
 
   useEffect( () => {
     
@@ -19,13 +22,25 @@ const StudentReleases = () => {
   
   return (
     <>
+
+      <TabMenu active={activeTab} tabs={tabOptions} activeOption={setActiveTab} >
         {
-          releases
+          activeTab == 1
+          ?
+          <ShowReleases releases={releases?.section} />
+          :
+          activeTab == 2
+          ?
+          <ShowReleases courseReleases={releases?.courses} />
+          :
+          activeTab == 3
           ?
           <ShowReleases releases={releases?.student} />
           :
           null
         }
+
+      </TabMenu>
     </>
   )
 }
