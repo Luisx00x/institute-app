a1b4e351b7017e5c535466280ba508603c062208
Luisx00x <lmarcano203@gmail.com>
2023-08-03 06:59:00 -0500

    setting te alternative option to parents change child

--- src/app/parents/[student]/layout.js
+++ src/app/parents/[student]/layout.js
@@ -2,16 +2,12 @@
 import { usePathname } from 'next/navigation';
 import s from './layout.module.css';
 import Sidebar from '@/components/sidebar/Sidebar';
-import { useSelector } from 'react-redux';
-
 
 const layout = ({children}) =>{
 
   const path = usePathname();
 
   const param = path.split("/")[2]
-
-  const students = useSelector(state => state.parent.parentStudents);
 
   const menuRoutes = [
     {name: "Inicio", route: `/parents/${param}`},
@@ -20,20 +16,14 @@ const layout = ({children}) =>{
     {name: "Ver libreta de calificaciones", route: `/parents/${param}/reportCard`},
   ]
 
-  const AltermenuRoutes = [
-    {name: "Inicio", route: `/parents/${param}`},
-    {name: "Horario", route: `/parents/${param}/schedules`},
-    {name: "Ver comunicados", route: `/parents/${param}/releases`},
-    {name: "Ver libreta de calificaciones", route: `/parents/${param}/reportCard`},
-    {name: "Seleccionar hijo", route: `/parents`}
-  ]
+  const AlterRoute = {name: "Seleccionar hijo", route: `/parents`}
 
   return (
     <>
     {
       <div className={s.container}>
         <div className={s.uiContainer}>
-          <Sidebar menus={students.length > 1 ? AltermenuRoutes : menuRoutes} />
+          <Sidebar menus={menuRoutes} altMenu={AlterRoute} />
             {children}
         </div>
       </div>
