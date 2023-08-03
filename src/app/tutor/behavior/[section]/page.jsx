bc470d8ad9a3ba8533629a3b308c60757f267cdc
Luisx00x <lmarcano203@gmail.com>
2023-08-03 10:41:11 -0500

    tutor/behavior/section is obteining students correctly

--- src/app/tutor/behavior/[section]/page.jsx
+++ src/app/tutor/behavior/[section]/page.jsx
@@ -1,10 +1,22 @@
+'use client'
+import { useEffect, useState } from "react";
+import { sectionSelected } from "../../tutorHandlers";
 
-const TutorSectionSelect = () => {
+const TutorSectionSelect = ({params}) => {
+
+  const sectionId = params.section;
+  const [section, setSection] = useState();
 
   
-
+  useEffect( () => {
+    
+    sectionSelected(setSection, sectionId)
+    
+  },[])
+  
   return (
     <>
+    {console.log(section)}
 
         <h3>Behavior</h3>
 
--- src/app/tutor/tutorHandlers.js
+++ src/app/tutor/tutorHandlers.js
@@ -2,6 +2,7 @@ import useFetch from "@/Hooks/useFetch"
 import { setTutorSections } from "@/redux/slice";
 const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL;
 const TUTOR_SECTIONS = process.env.NEXT_PUBLIC_TUTOR_SECTIONS;
+const SECTION_STUDENTS = process.env.NEXT_PUBLIC_SECTION_STUDENTS;
 
 export const tutorSections = (dispatch, userId) => {
   
@@ -15,6 +16,25 @@ export const tutorSections = (dispatch, userId) => {
   .then( res => {
     if(status == 200 || status == 304) return dispatch(setTutorSections(res));
     throw new Error(res)
+  })
+  .catch(err => {
+    console.error(err)
+  })
+
+}
+
+export const sectionSelected = (setData, sectionId) => {
+
+  let status;
+
+  useFetch(`${LOGIN_URL}/${SECTION_STUDENTS}?sectionId=${sectionId}`)
+  .then( res => {
+    status = res.status;
+    return res.json();
+  })
+  .then( res => {
+    if(status == 200 || status == 304) return setData( prev => res.Students);
+    throw new Error(res);
   })
   .catch(err => {
     console.error(err)
