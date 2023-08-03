297004605dde8028c4c75c8de4726168be56c4e3
Luisx00x <lmarcano203@gmail.com>
2023-08-03 05:43:55 -0500

    creaning parent interface before logout

--- src/components/header/headerHandler.js
+++ src/components/header/headerHandler.js
@@ -1,5 +1,12 @@
-import { setUser } from "@/redux/slice";
+import { setParentInfo, setParentStudents, setReleases, setReport, setSchedules, setSectionInfo, setStudentUId, setUser } from "@/redux/slice";
 
 export const logout = (dispatch) => {
   dispatch(setUser(null));
+  dispatch(setSectionInfo(null));
+  dispatch(setSchedules(null));
+  dispatch(setReleases(null));
+  dispatch(setParentInfo(null));
+  dispatch(setParentStudents(null));
+  dispatch(setStudentUId(null))
+
 }
