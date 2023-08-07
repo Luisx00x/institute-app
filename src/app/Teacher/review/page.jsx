6892f49dee4f69caa9d265d6a45a426b5713e08c
Luisx00x <lmarcano203@gmail.com>
2023-08-07 15:48:36 -0500

    updating new view review for sumbit homeworks review

--- src/app/Teacher/review/page.jsx
+++ src/app/Teacher/review/page.jsx
@@ -1,8 +1,35 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { callHomeworksAnswers } from "./reviewHandler";
+import StudentsHomework from "@/components/StudentsHomework/StudentsHomework";
+import s from './review.module.css';
 
 const SetReviews = () => {
+  
+  const { homeworkId } = useSelector(state => state.teacher.reviewsParam);
+  const [homework, setHomework] = useState();
+  const modal = useSelector(state => state.primarySlice.modal);
+
+  useEffect( () => {
+
+    callHomeworksAnswers(setHomework, homeworkId);
+
+  },[homeworkId, modal])
+
+
   return (
     <>
-      review
+
+      <div className={s.answerContainer}>
+
+        <h2>Asignación: {homework?.asignation}</h2>
+
+        <StudentsHomework students={homework?.HomeworksAnswers} />
+
+      </div>
+
 
     </>
   )
